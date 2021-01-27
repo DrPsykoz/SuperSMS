15dc38af26acb2eadba902811107bf383972c6c4
DrPsykoz <theo.frvl@gmail.com>
2021-01-27 11:13:43 +0100

    Request SMS RECEIVE

--- app/src/main/java/fr/theofreville/supersms/MainActivity.java
+++ app/src/main/java/fr/theofreville/supersms/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions(new String[]{"android.permission.RECEIVE_SMS"},007);
     }
 }
