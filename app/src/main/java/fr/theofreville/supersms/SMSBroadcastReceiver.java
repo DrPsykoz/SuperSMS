945bac324518314da8f440cadbee34792d31e6d6
DrPsykoz <theo.frvl@gmail.com>
2021-01-27 10:42:47 +0100

    dzdzd

--- app/src/main/java/fr/theofreville/supersms/SMSBroadcastReceiver.java
+++ app/src/main/java/fr/theofreville/supersms/SMSBroadcastReceiver.java
@@ -3,13 +3,42 @@ package fr.theofreville.supersms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SMSBroadcastReceiver extends BroadcastReceiver {
 
+    final SmsManager sms = SmsManager.getDefault();
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+
+        // Retrieves a map of extended data from the intent.
+        final Bundle bundle = intent.getExtras();
+        try {
+            if (bundle != null) {
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
+
+                    Toast.makeText(context, "senderNum: "+ senderNum + ", message: " + message, Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" +e);
+        }
     }
 }
